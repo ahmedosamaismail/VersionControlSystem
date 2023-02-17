7fca7d93237b43d90b9237c9d1ce6b0cef939140
Ahmed Osama Ismail <ahmedlogn.ahmed@gmail.com>
2023-02-17 11:13:36 +0200

    Added a vector to print getSum output

--- main.cpp
+++ main.cpp
@@ -10,5 +10,7 @@ int getSum(vector<int> numbers)
 }
 int main()
 {
+    vector <int> numbers = {0 ,1 ,2 , 3 , 4 , 5 , 6 , 7 , 8 , 9};
+    cout << "The sum is : " << getSum(numbers) << endl;
     return 0;
 }
