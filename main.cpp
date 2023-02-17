8c6b1d9b46379b5540973a4f708cf524d4ee875f
Ahmed Osama Ismail <ahmedlogn.ahmed@gmail.com>
2023-02-17 11:34:17 +0200

    getAverage function and it's output

--- main.cpp
+++ main.cpp
@@ -8,9 +8,16 @@ int getSum(vector<int> numbers)
     int sum = accumulate(numbers.begin(), numbers.end(), 0);
     return sum;
 }
+
+double getAverage(vector<int> numbers)
+{
+    double avg = double(getSum(numbers)) / numbers.size();
+    return avg;
+}
 int main()
 {
-    vector <int> numbers = {0 ,1 ,2 , 3 , 4 , 5 , 6 , 7 , 8 , 9};
+    vector<int> numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     cout << "The sum is : " << getSum(numbers) << endl;
+    cout << "The average is : " << getAverage(numbers) << endl;
     return 0;
 }
