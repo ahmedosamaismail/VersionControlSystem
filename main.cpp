84192df9ea593b865d32fd166f5e0837a17f4b5a
Ahmed Osama Ismail <ahmedlogn.ahmed@gmail.com>
2023-02-17 11:43:22 +0200

    getMin function and it's output

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
+#include <bits/stdc++.h>
 using namespace std;
 
 int getSum(vector<int> numbers)
@@ -8,9 +9,15 @@ int getSum(vector<int> numbers)
     int sum = accumulate(numbers.begin(), numbers.end(), 0);
     return sum;
 }
+
+int getMin(vector<int> numbers){
+    int min = *min_element(numbers.begin(), numbers.end()) ;
+    return min ;
+}
 int main()
 {
     vector <int> numbers = {0 ,1 ,2 , 3 , 4 , 5 , 6 , 7 , 8 , 9};
     cout << "The sum is : " << getSum(numbers) << endl;
+    cout << "The minimum element in the vector is : " << getMin(numbers) << endl ;
     return 0;
 }
